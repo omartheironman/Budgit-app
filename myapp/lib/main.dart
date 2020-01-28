97aef3804cfe92529b1bc31b44986125fb9f65e9
Omar Moustafa Moharrem <omar.moharrem@unity3d.com>
2020-01-28 15:51:04 -0500

    Started mock up design

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -14,45 +14,279 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
+      backgroundColor: Colors.grey[900],
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       appBar: AppBar(
-        title: Text("Infinit Wallet"),
-        backgroundColor: Colors.green,
+        title: Text("MyWallet"),
+        backgroundColor: Colors.grey[850],
+        centerTitle: true,
+        elevation: 0.0,
       ),
 
 
-      body: Column(
-      //  mainAxisAlignment: MainAxisAlignment.centes,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: <Widget>[
-      Container(
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
 
-        padding: EdgeInsets.all(20.0),
-        alignment: Alignment.center,
-            child: Text(
-              'Take control of your finances!',
-                style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
                 letterSpacing: 2.0,
-                    color: Colors.green,
-                fontFamily: 'Customfont'
-              ),
+                //fontSize: 20.0,
+              )
             ),
-      ),
 
-        Container(
+            SizedBox(height: 10.0),
+            Text(
+                'Omar Moharrem',
+                style: TextStyle(
+                  color: Colors.greenAccent,
+                  letterSpacing: 2.0,
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold
+                )
+            ),
 
-            alignment: Alignment.center,
+            SizedBox(height: 10.0),
+            Text(
+                'Left to Spend',
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                  //fontSize: 20.0,
+                )
+            ),
+            SizedBox(height: 10.0),
 
-            //padding: EdgeInsets.all(50.0),
-           child:  FlatButton(
-            onPressed: (){},
-            color: Colors.green,
-            child: Text('Add'),
-          )
-    ),
-        ],
+            Text(
+                "\$521",
+                style: TextStyle(
+                    color: Colors.greenAccent,
+                    letterSpacing: 2.0,
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold
+                )
+            ),
 
+            SizedBox(height: 24.0),
+
+            Row(
+              children: <Widget>[
+                Icon(
+                  //Icons.email,
+                  Icons.account_balance,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 248.0),
+                Text(
+                    "Shopping",
+                    style: TextStyle(
+                        color: Colors.grey,
+                        letterSpacing: 2.0,
+                        fontSize: 15.0,
+                    )
+                ),
+              ]
+            ),
+            //SizedBox(height: 10.0), //Sized Box
+            Row(
+                children: <Widget>[
+                  SizedBox(width: 290.0),
+
+                  Text(
+                      "\$100",
+                      style: TextStyle(
+                          color: Colors.greenAccent,
+                          letterSpacing: 2.0,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold
+                      )
+                  ),
+                ]
+            ),
+            SizedBox(height: 10.0), //Sized Box
+
+            Row(
+                children: <Widget>[
+                  Icon(
+                    //Icons.email,
+                    Icons.account_balance,
+                    color: Colors.grey[400],
+                  ),
+                  SizedBox(width: 260.0),
+                  Text(
+                      "Dinning",
+                      style: TextStyle(
+                        color: Colors.grey,
+                        letterSpacing: 2.0,
+                        fontSize: 15.0,
+                      )
+                  ),
+                ]
+            ),
+            Row(
+                children: <Widget>[
+                  SizedBox(width: 300.0),
+
+                  Text(
+                      "\$80",
+                      style: TextStyle(
+                          color: Colors.greenAccent,
+                          letterSpacing: 2.0,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold
+                      )
+                  ),
+                ]
+            ),
+
+            SizedBox(height: 10.0), //Sized Box
+            Row(
+                children: <Widget>[
+                  Icon(
+                    //Icons.email,
+                    Icons.account_balance,
+                    color: Colors.grey[400],
+                  ),
+                  SizedBox(width: 290.0),
+                  Text(
+                      "Fun",
+                      style: TextStyle(
+                        color: Colors.grey,
+                        letterSpacing: 2.0,
+                        fontSize: 15.0,
+                      )
+                  ),
+                ]
+            ),
+            Row(
+                children: <Widget>[
+                  SizedBox(width: 300.0),
+
+                  Text(
+                      "\$40",
+                      style: TextStyle(
+                          color: Colors.greenAccent,
+                          letterSpacing: 2.0,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold
+                      )
+                  ),
+                ]
+            ),
+            SizedBox(height: 10.0), //Sized Box
+            Row(
+                children: <Widget>[
+                  Icon(
+                    //Icons.email,
+                    Icons.account_balance,
+                    color: Colors.grey[400],
+                  ),
+                  SizedBox(width: 270.0),
+                  Text(
+                      "Sports",
+                      style: TextStyle(
+                        color: Colors.grey,
+                        letterSpacing: 2.0,
+                        fontSize: 15.0,
+                      )
+                  ),
+                ]
+            ),
+            Row(
+                children: <Widget>[
+                  SizedBox(width: 300.0),
+
+                  Text(
+                      "\$69",
+                      style: TextStyle(
+                          color: Colors.greenAccent,
+                          letterSpacing: 2.0,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold
+                      )
+                  ),
+                ]
+            ),
+
+            SizedBox(height: 10.0), //Sized Box
+            Row(
+                children: <Widget>[
+                  Icon(
+                    //Icons.email,
+                    Icons.account_balance,
+                    color: Colors.grey[400],
+                  ),
+                  SizedBox(width: 270.0),
+                  Text(
+                      "Phone",
+                      style: TextStyle(
+                        color: Colors.grey,
+                        letterSpacing: 2.0,
+                        fontSize: 15.0,
+                      )
+                  ),
+                ]
+            ),
+            Row(
+                children: <Widget>[
+                  SizedBox(width: 300.0),
+
+                  Text(
+                      "\$21",
+                      style: TextStyle(
+                          color: Colors.greenAccent,
+                          letterSpacing: 2.0,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold
+                      )
+                  ),
+                ]
+            ),
+            SizedBox(height: 10.0), //Sized Box
+            Row(
+                children: <Widget>[
+                  Icon(
+                    //Icons.email,
+                    Icons.account_balance,
+                    color: Colors.grey[400],
+                  ),
+                  SizedBox(width: 290.0),
+                  Text(
+                      "Car",
+                      style: TextStyle(
+                        color: Colors.grey,
+                        letterSpacing: 2.0,
+                        fontSize: 15.0,
+                      )
+                  ),
+                ]
+            ),
+            Row(
+                children: <Widget>[
+                  SizedBox(width: 300.0),
+
+                  Text(
+                      "\$11",
+                      style: TextStyle(
+                          color: Colors.greenAccent,
+                          letterSpacing: 2.0,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold
+                      )
+                  ),
+                ]
+            ),
+
+
+          ],
+
+        ),
       ),
 
 //      body: Center(
@@ -73,23 +307,43 @@ class Home extends StatelessWidget {
 
      // ),
 
-
-
-
+//
+//      bottomNavigationBar: BottomAppBar(
+//        shape: const CircularNotchedRectangle(),
+//        child: Container(
+//          height: 50.0,
+//          color: Colors.grey,
+//        ),
+//      ),
       floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.greenAccent,
+        child: const Icon(
+          Icons.edit,
+          color: Colors.blueGrey,
+        ),
         onPressed: () {},
-        child: Text("Add"),
-        backgroundColor: Colors.green,
       ),
 
       bottomNavigationBar: BottomAppBar(
-        shape: const CircularNotchedRectangle(),
-        child: Container(
-          height: 50.0,
+        shape: CircularNotchedRectangle(),
+        child: new Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.menu),
+              color: Colors.white,
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.search),
+              color: Colors.white,
+              onPressed: () {},
+            ),
+          ],
         ),
+        color: Colors.grey,
       ),
-
-
     );
   }
 }
